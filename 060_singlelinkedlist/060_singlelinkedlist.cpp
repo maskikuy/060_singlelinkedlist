7d8f645b50e3761d36c6cf5fdf71ff9c8e41113c
maskikuy <kproject305@gmail.com>
2024-04-26 14:39:32 +0700

    finish project

--- 060_singlelinkedlist/060_singlelinkedlist.cpp
+++ 060_singlelinkedlist/060_singlelinkedlist.cpp
@@ -13,9 +13,9 @@ void addNode() {
     int nim;
     string nama;
     Node* nodeBaru = new Node();
-    cout << "Masukkan NIM: ";
+    cout << "Masukkan NIM : ";
     cin >> nim;
-    cout << "Masukkan Nama: ";
+    cout << "Masukkan Nama : ";
     cin >> nama;
     nodeBaru->noMhs = nim;
     nodeBaru->name = nama;
@@ -45,82 +45,88 @@ void addNode() {
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
-    bool serachNode(int nim, Node * current, Node * previous) {
-        previous = START;
-        current = START;
-        while (current != NULL && nim > current->noMhs)
-        {
-            previous = current;
-            current = current->next;
-        }
-        if (current == NULL)
-        {
-            return false;
-        }
-        else if (current->noMhs == nim)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+}
+
+bool serachNode(int nim, Node* current, Node* previous) {
+    previous = START;
+    current = START;
+    while (current != NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
     }
-    bool deleteNode(int nim) {
-        Node* current = START;
-        Node* previous = START;
-        if (serachNode(nim, previous, current) == false)
-            return false;
-        previous->next = current->next;
-        if (current == START)
-            START = current->next;
+
+    if (current == NULL)
+    {
+        return false;
+    }
+    else if (current->noMhs == nim)
+    {
         return true;
     }
-    bool listEmpty() {
-        if (START == NULL)
-            return true;
-        else
-            return false;
+    else
+    {
+        return false;
+    }
+}
 
-    }
-    void traverse() {
-        if (listEmpty()) {
-            cout << "List Kosong" << endl;
-            system("pause");
-            system("cls");
-            return;
-        }
-        else {
-            Node* currentNode = START;
-            while (currentNode != NULL) {
-                cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
-                currentNode = currentNode->next;
-            }
-        }
-    }
-    void searchData() {
-        if (listEmpty()) {
-            cout << "List Kosong" << endl;
-            system("pause");
-            system("cls");
-            return;
-        }
-        else {
-            int nim;
-            cout << "Masukkan NIM: ";
-            cin >> nim;
-            Node* currentNode = START;
-            while (currentNode != NULL) {
-                if (currentNode->noMhs == nim) {
-                    cout << "NIM: " << currentNode->noMhs << ",Nama: " << currentNode->name << endl;
-                    return;
-                }
-                currentNode = currentNode->next;
-            }
-            cout << "Data tidak ditemukan" << endl;
-        }
-    }
+bool deleteNode(int nim) {
+    Node* current = START;
+    Node* previous = START;
+    if (serachNode(nim, previous, current) == false)
+        return false;
+    previous->next = current->next;
+    if (current == START)
+        START = current->next;
+    return true;
+}
 
+bool listEmpty() {
+    if (START == NULL)
+        return true;
+    else
+        return false;
+}
+
+void traverse() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else {
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << "NIM : " << currentNode->noMhs << ", Nama : " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
+void searchData() {
+    if (listEmpty()) {
+        cout << "List kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
+    else {
+        int nim;
+        cout << "Masukkan NIM : ";
+        cin >> nim;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            if (currentNode->noMhs == nim) {
+                cout << "NIM : " << currentNode->noMhs << ", Nama: " << currentNode->name <<
+                    endl;
+                return;
+            }
+            currentNode = currentNode->next;
+        }
+        cout << "Data tidak ditemukan" << endl;
+    }
+}
 int main() {
     int pilihan;
     do
@@ -143,5 +149,44 @@ int main() {
                 system("pause");
                 system("cls");
                 break;
+            case 2:
+                if (listEmpty())
+                {
+                    cout << "List Kosong" << endl;
+                    system("pause");
+                    system("cls");
+                    break;
+                }
 
-     
+                int nim;
+                cout << "Masukkan NIM: ";
+                cin >> nim;
+                if (deleteNode(nim)) {
+                    cout << "nim: " << nim << " berhasil dihapus" << endl;
+                    system("pause");
+                    system("cls");
+                }
+                else
+                    cout << "Data tidak ditemukan" << endl;
+                break;
+            case 3:
+                traverse();
+                break;
+            case 4:
+                searchData();
+                break;
+            case 5:
+                break;
+            default:
+                cout << "Pilihan tidak ada" << endl;
+                break;
+            }
+        }
+        catch (exception e)
+        {
+            cout << "Terjadi kesalahan" << endl;
+        }
+
+    } while (pilihan != 5);
+}
+      
