ff133f20fd1fdb13a3b3c843e4dd69319bb01f58
maskikuy <kproject305@gmail.com>
2024-04-26 14:08:45 +0700

    masukan booldelete node

--- 060_singlelinkedlist/060_singlelinkedlist.cpp
+++ 060_singlelinkedlist/060_singlelinkedlist.cpp
@@ -56,3 +56,24 @@ void addNode() {
         if (current == NULL)
         {
             return false;
+        }
+        else if (current->noMhs == nim)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    bool deleteNode(int nim) {
+        Node* current = START;
+        Node* previous = START;
+        if (serachNode(nim, previous, current) == false)
+            return false;
+        previous->next = current->next;
+        if (current == START)
+            START = current->next;
+        return true;
+    }
+
